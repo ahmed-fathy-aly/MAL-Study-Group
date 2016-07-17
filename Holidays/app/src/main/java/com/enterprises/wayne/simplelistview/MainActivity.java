7fc9e8375345fb92a66e0e924e5c9d539075e176
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-07-17 20:43:46 +0300

    added options menu empty methods to starter code

--- Holidays/app/src/main/java/com/enterprises/wayne/simplelistview/MainActivity.java
+++ Holidays/app/src/main/java/com/enterprises/wayne/simplelistview/MainActivity.java
@@ -2,6 +2,9 @@ package com.enterprises.wayne.simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -38,6 +41,19 @@ public class MainActivity extends AppCompatActivity
 
         // bind the adapter to the list view
         mListViewNumbers.setAdapter(mAdapterNumbers);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return super.onOptionsItemSelected(item);
     }
 
     /**
