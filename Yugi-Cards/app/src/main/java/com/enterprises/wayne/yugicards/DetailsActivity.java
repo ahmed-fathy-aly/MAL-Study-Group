7c47a1ad31e7963a586b3ba1e0c129fe85ab6659
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-07-25 22:48:38 +0300

    fix home and label for details activity

--- Yugi-Cards/app/src/main/java/com/enterprises/wayne/yugicards/DetailsActivity.java
+++ Yugi-Cards/app/src/main/java/com/enterprises/wayne/yugicards/DetailsActivity.java
@@ -2,7 +2,6 @@ package com.enterprises.wayne.yugicards;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,9 +18,8 @@ public class DetailsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle(getString(R.string.card_details));
+        getSupportActionBar().setHomeButtonEnabled(true);
+        //getSupportActionBar().setTitle(getString(R.string.card_details));
 
         // reference view
         textViewTitle = (TextView) findViewById(R.id.text_view_title);
@@ -33,14 +31,5 @@ public class DetailsActivity extends AppCompatActivity
         textViewDescription.setText("Card Description");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        if (item.getItemId() == android.R.id.home)
-        {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
 }
