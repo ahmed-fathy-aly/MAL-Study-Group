905b1d8a24366f54a9e911bc1fae12a08d939395
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-07-25 22:16:30 +0300

    add cards to a list view

--- Yugi-Cards/app/src/main/java/com/enterprises/wayne/yugicards/MainActivity.java
+++ Yugi-Cards/app/src/main/java/com/enterprises/wayne/yugicards/MainActivity.java
@@ -3,11 +3,14 @@ package com.enterprises.wayne.yugicards;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -15,11 +18,30 @@ public class MainActivity extends AppCompatActivity
     /* constants */
     public final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    /* UI */
+    ListView listViewCards;
+    ArrayAdapter<String> adapterCards;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // reference the list view
+        listViewCards = (ListView) findViewById(R.id.list_view_cards);
+
+        // create an adapter with empty data
+        ArrayList<String> emptyData = new ArrayList<>();
+        adapterCards = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                emptyData
+        );
+
+        // bind the adapter to the list view
+        listViewCards.setAdapter(adapterCards);
 
         loadData();
     }
@@ -54,6 +76,20 @@ public class MainActivity extends AppCompatActivity
 
                         // parse the data
                         List<Card> cards = ParsingUtils.parseResponse(result);
+                        if (cards == null)
+                        {
+                            Toast.makeText(MainActivity.this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        // convert to a list of strings
+                        List<String> cardsString = new ArrayList<String>();
+                        for (Card card : cards)
+                            cardsString.add(card.getTitle());
+
+                        // add to the adapter
+                        adapterCards.clear();
+                        adapterCards.addAll(cardsString);
                     }
                 });
     }
